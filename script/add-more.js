c108642930de84fb6c4716152ee513216007dedf
Sergio Herrera Rivera <sh0041314@gmail.com>
2024-10-06 10:35:49 -0600

    eliminacion de basura

--- script/add-more.js
+++ script/add-more.js
@@ -1,29 +1,4 @@
-// // add-more.js
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const agregarMasBtn = document.getElementById('agregar-mas');
-
-//     // Función para manejar el almacenamiento de opciones adicionales antes de redirigir
-//     function handleAgregarMas() {
-//         const adicionales = document.querySelectorAll('.adicional-checkbox');
-//         const adicionalesSeleccionados = [];
-
-//         adicionales.forEach(adicional => {
-//             if (adicional.checked) {
-//                 adicionalesSeleccionados.push({
-//                     text: adicional.parentElement.textContent.trim(),
-//                     price: parseInt(adicional.getAttribute('data-price'), 10)
-//                 });
-//                 adicional.checked = false; // Desmarcar el checkbox después de seleccionarlo
-//             }
-//         });
-
-//         localStorage.setItem('adicionales', JSON.stringify(adicionalesSeleccionados));
-//         window.location.href = 'index.html#menu'; // Redirige a la sección del menú en index.html
-//     }
-
-//     agregarMasBtn.addEventListener('click', handleAgregarMas);
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
     const agregarMasBtn = document.getElementById('agregar-mas');
