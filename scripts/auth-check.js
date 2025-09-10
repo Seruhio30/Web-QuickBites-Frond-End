43917b3e081e3ff1604624daa61382ba39885dcf
Seruhio30 <sh0041314@gmail.com>
2025-09-09 22:57:20 -0600

    limpieza de archivos

--- scripts/auth-check.js
+++
@@ -1,9 +0,0 @@
-// auth-check.js
-document.addEventListener("DOMContentLoaded", function() {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-        alert("Por favor, inicia sesión primero.");
-        window.location.href = "login.html"; // Redirige a la página de inicio de sesión
-    }
-});
