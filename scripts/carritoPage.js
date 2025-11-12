858a09b5e2c54ec7b8c13ec5763783d698e36461
Seruhio30 <sh0041314@gmail.com>
2025-11-11 18:46:37 -0600

    reajuste en responsive en el menu

--- scripts/carritoPage.js
+++ scripts/carritoPage.js
@@ -1,4 +1,7 @@
 import { initCarrito } from "./carrito.js";
+import { navigation } from "./navigation.js";
+
+navigation();
 
 document.addEventListener("DOMContentLoaded", () => {
   initCarrito();
